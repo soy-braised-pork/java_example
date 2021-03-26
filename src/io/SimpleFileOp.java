e8efe7c9091d81466d62c949dfa555042b2884dc
soy-braised-pork <2428784410@qq.com>
2021-03-26 17:59:49 +0800

    update

--- src/io/SimpleFileOp.java
+++ src/io/SimpleFileOp.java
@@ -92,22 +92,60 @@ public class SimpleFileOp {
             String tempString = null;
             int line = 1;
             //一次读入一行，直到读入null为文件结果
-            while ((tempString=reader.readLine())!=null){
+            while ((tempString = reader.readLine()) != null) {
                 //显示行号
-                System.out.println("Line"+line+":"+tempString);
+                System.out.println("Line" + line + ":" + tempString);
                 line++;
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if (reader!=null){
+        } finally {
+            if (reader != null) {
                 try {
                     reader.close();
-                }catch (IOException e1){}
+                } catch (IOException e1) {
+                }
             }
         }
     }
+
+    /**
+     * 随机读取文件内容
+     * nio
+     */
+    public static void readFileByRandomAccess(String fileName) {
+        RandomAccessFile randomFile = null;
+        try {
+            System.out.println("随机读取一段文件内容：");
+            //打开一个随机访问文件流，按只读方式
+            randomFile = new RandomAccessFile(fileName, "r");
+            //文件长度，字节数
+            long fileLength = randomFile.length();
+            //读文件的起始位置
+            int beginIndex = (fileLength > 4) ? 4 : 0;
+            //将读文件的开始位置移到beginIndex位置
+            randomFile.seek(beginIndex);
+            byte[] bytes=new byte[10];
+            int byteread=0;
+            //一次读10个字节，如果文件内容不足10字节，则读剩下的字节
+            //将一次读取的字节数赋给byteread
+            while ((byteread=randomFile.read(bytes))!=-1){
+                System.out.write(bytes,0,byteread);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(randomFile!=null){
+                try {
+                    randomFile.close();
+                }catch (IOException e1){
+
+                }
+            }
+        }
+    }
+
 
     /**
      * 显示输入流中还剩的字节数
