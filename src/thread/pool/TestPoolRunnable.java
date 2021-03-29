40975723c2e22811943b249b07f44a9814e31554
soy-braised-pork <2428784410@qq.com>
2021-03-29 22:01:24 +0800

    update

---
+++ src/thread/pool/TestPoolRunnable.java
@@ -0,0 +1,14 @@
+package thread.pool;
+
+public class TestPoolRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+"----->"+Thread.currentThread().getId());
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/thread/pool/ThreadPoolCore.java
@@ -0,0 +1,4 @@
+package thread.pool;
+
+public class ThreadPoolCore {
+}
