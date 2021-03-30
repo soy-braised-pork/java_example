617f10640f83b3431a48212fa80b7d49834a89fe
soy-braised-pork <2428784410@qq.com>
2021-03-30 12:31:09 +0800

    update

---
+++ src/source/ConcurrentHashMapDemo.java
@@ -0,0 +1,10 @@
+package source;
+
+import java.io.Serializable;
+import java.util.AbstractMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ConcurrentHashMapDemo<K,V> extends AbstractMap<K,V> implements ConcurrentMap<K,V>, Serializable {
+
+
+}
