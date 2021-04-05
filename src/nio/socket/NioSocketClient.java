08a0862f9a5ea973f23a5f5708e9fac00f091823
soy-braised-pork <2428784410@qq.com>
2021-04-05 10:14:54 +0800

    update

--- src/nio/socket/NioSocketClient.java
+++ src/nio/socket/NioSocketClient.java
@@ -4,13 +4,44 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
 
 public class NioSocketClient {
-    public static void client() throws IOException {
+    public static void client() {
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         SocketChannel socketChannel = null;
-        socketChannel = SocketChannel.open();
-        socketChannel.configureBlocking(false);
-        socketChannel.connect(new InetSocketAddress("localhost", 10010));
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("localhost", 10010));
+            if (socketChannel.finishConnect()) {
+                int i = 0;
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = "I'm" + i++ + "-th information from client";
+                    byteBuffer.clear();
+                    byteBuffer.put(info.getBytes());
+                    byteBuffer.flip();
+                    while (byteBuffer.hasRemaining()) {
+                        System.out.println(byteBuffer);
+                        socketChannel.write(byteBuffer);
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socketChannel != null) {
+                    socketChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        client();
     }
 }
