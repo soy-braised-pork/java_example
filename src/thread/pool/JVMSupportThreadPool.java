8ff38a41cbf2d97a6a38b9069e9d687f863cc886
soy-braised-pork <2428784410@qq.com>
2021-04-14 09:01:22 +0800

    update

--- src/thread/pool/JVMSupportThreadPool.java
+++ src/thread/pool/JVMSupportThreadPool.java
@@ -9,18 +9,22 @@ import java.util.concurrent.TimeUnit;
 public class JVMSupportThreadPool {
     public static void main(String[] args) {
 
+        //核心线程数为1，最大值1
         //单线程的线程池。若多余一个任务被提交到该线程池，任务会被保存在一个任务队列中，
         //待线程空闲，按先入先出的顺序执行队列中的任务。
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
 
+        //核心线程数为0，最大值Integer.MAX_VALUE
         //不确定长度的线程池。但若有空闲线程可以复用，则会优先使用可复用的线程。
         // 若所有线程均在工作，又有新任务提交，则会创建新线程处理任务。所有线程在当前任务执行完毕后，将返回线程池进行复用。
         ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
 
+        //核心线程数、最大值可自行设置
         //定长线程池。当有一个新任务提交时，线程池中若有空闲线程，则立即执行。
         //若没有，则新任务会被暂存在一个任务队列中，待有线程空闲时，便处理在任务队列中的任务。
         ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(5);
 
+        //核心线程数为自行设置，最大值Integer.MAX_VALUE
         //周期性、定时执行任务的线程池
         ScheduledExecutorService newScheduledThreadPool = Executors.newScheduledThreadPool(5);
 
