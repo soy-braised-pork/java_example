1007445af9c8d8b3776693ff5ea4237b1c8596cd
soy-braised-pork <2428784410@qq.com>
2021-04-27 21:25:26 +0800

    update

--- src/queue/QueueBase.java
+++ src/queue/QueueBase.java
@@ -1,4 +1,36 @@
 package queue;
 
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public class QueueBase {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("---------------ArrayBlockingQueue--------------");
+        //阻塞队列  有长度的队列
+        ArrayBlockingQueue<String> array = new ArrayBlockingQueue<>(5);
+        array.put("a");
+        array.put("b");
+        array.add("c");
+        array.add("d");
+        array.add("e");
+        //返回一个布尔类型 在3秒之内能不能加入 不能返回false
+        System.out.println(array.offer("a", 3, TimeUnit.SECONDS));
+        System.out.println("所有数据  >>  " + array.toString());
+
+
+        System.out.println("---------------LinkedBlockingQueue---------------");
+        //阻塞队列  无长度限制队列
+        LinkedBlockingQueue<String> linked = new LinkedBlockingQueue<>();
+        linked.offer("a");
+        linked.offer("b");
+        linked.offer("c");
+        linked.offer("d");
+        linked.add("f");
+        System.out.println("总长度 >> "+array.size());
+        for (Iterator iterator)
+
+    }
 }
--- src/queue/UsePriorityBlockingQueue.java
+++ src/queue/UsePriorityBlockingQueue.java
@@ -3,7 +3,7 @@ package queue;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class UsePriorityBlockingQueue {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         PriorityBlockingQueue<Task> q2 = new PriorityBlockingQueue<>();
 
@@ -15,11 +15,11 @@ public class UsePriorityBlockingQueue {
         t2.setId(4);
         t2.setName("id为4");
 
-        Task t3=new Task();
+        Task t3 = new Task();
         t3.setId(1);
         t3.setName("id为1");
 
-        Task t4=new Task();
+        Task t4 = new Task();
         t4.setId(2);
         t4.setName("id为2");
 
@@ -28,6 +28,13 @@ public class UsePriorityBlockingQueue {
         q2.add(t3);
         q2.add(t4);
 
-        //
+        //第一次取值的时候是取最小的，后面的不做排序
+        System.out.println("容器：" + q2);  //[1,id为1, 2,id为2, 3,id为3, 4,id为4]
+        //拿出一个元素后 又会取一个最小的出来 放在第一个
+        System.out.println(q2.take().getId());
+        System.out.println("容器：" + q2);   //[2,id为2, 4,id为4, 3,id为3]
+        System.out.println(q2.take().getId());
+        System.out.println("容器：" + q2);  //[3,id为3, 4,id为4]
+
     }
 }
