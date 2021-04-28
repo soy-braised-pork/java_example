085d3507c55f953ae37fbb3ba4eefccc42f31060
soy-braised-pork <2428784410@qq.com>
2021-04-28 09:14:03 +0800

    update

--- src/queue/QueueBase.java
+++ src/queue/QueueBase.java
@@ -1,8 +1,11 @@
 package queue;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
 public class QueueBase {
@@ -29,8 +32,43 @@ public class QueueBase {
         linked.offer("c");
         linked.offer("d");
         linked.add("f");
-        System.out.println("总长度 >> "+array.size());
-        for (Iterator iterator)
+        System.out.println("总长度 >> " + array.size());
+        for (Iterator iterator = linked.iterator(); iterator.hasNext(); ) {
+            String string = (String) iterator.next();
+            System.out.println(string + "--");
+        }
+        System.out.println();
+        List<String> list = new ArrayList<>();
+        //在q的队列中取三个元素放到list队列里
+        System.out.println(linked.drainTo(list, 3));
+        System.out.println("取出LinkedBlockingQueue数据放到list列表的长度为 >> " + list.size());
+        for (String string : list) {
+            System.out.println(string + "--");
+        }
+        System.out.println();
 
+
+        System.out.println("---------------SynchronousQueue---------------");
+        System.currentTimeMillis();
+        final SynchronousQueue<String> synchronousQueue = new SynchronousQueue<>();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "取数据" + synchronousQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronousQueue.add("b");
+                System.out.println(Thread.currentThread().getName() + "加入数据");
+            }
+        });
+        thread1.start();
     }
 }
