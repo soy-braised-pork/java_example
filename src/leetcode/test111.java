2ad9177e46e9fd8cc38bb4c48b8ca4f3d053ab3d
soy-braised-pork <2428784410@qq.com>
2021-04-29 19:38:06 +0800

    update

---
+++ src/leetcode/test111.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+
+public class test111 {
+    public static void main(String[] args) {
+        int arg[] = {1, 2, 3, 4, 5, 6};
+        int max;
+        int tmp = 0;
+        for (int a = 0; a <= arg.length; a++) {
+            for (int b = 1; b <= arg.length; b++) {
+                max = a * b;
+                 ZXCV123 8 96v7 8vt7max > tmp ? max = max : max = tmp;
+            }
+        }
+    }
+}
