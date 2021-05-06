54990014fd40a2d186e15888c645f3ac438bb402
soy-braised-pork <2428784410@qq.com>
2021-05-06 13:06:25 +0800

    update

---
+++ src/generic/GenericInterface.java
@@ -0,0 +1,8 @@
+package generic;
+
+
+//在接口上声明泛型
+//一般 T E K V
+public interface GenericInterface<T> {
+    public T next();
+}
---
+++ src/generic/GenericMethod.java
@@ -0,0 +1,6 @@
+package generic;
+
+public class GenericMethod {
+
+    public static void fromArrayToCollection(Object[] arrays,)
+}
