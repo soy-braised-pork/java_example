465bbb3eef521dcf2976dc2acd7f55bb8299e284
soy-braised-pork <2428784410@qq.com>
2021-05-09 08:57:59 +0800

    update

--- src/nio/file/FileStreamIO.java
+++ src/nio/file/FileStreamIO.java
@@ -1,17 +1,30 @@
 package nio.file;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class FileStreamIO {
     public static void main(String[] args) {
         InputStream in = null;
         try {
-            in=new BufferedInputStream(new FileInputStream("src/nomal_io.txt"));
-        } catch (FileNotFoundException e) {
+            in = new BufferedInputStream(new FileInputStream("src/nomal_io.txt"));
+            byte[] buf = new byte[1024];
+            int bytesRead = in.read(buf);
+            while (bytesRead != -1) {
+                for (int i = 0; i < bytesRead; i++) {
+                    System.out.println(buf[i]);
+                }
+                bytesRead = in.read(buf);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
