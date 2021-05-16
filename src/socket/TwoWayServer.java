28962232bae5d528f6ef9fc3f806924778a532f1
soy-braised-pork <2428784410@qq.com>
2021-05-16 16:57:11 +0800

    update

--- src/socket/TwoWayServer.java
+++ src/socket/TwoWayServer.java
@@ -1,4 +1,18 @@
 package socket;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TwoWayServer {
+    public static void main(String[] args) throws IOException {
+        // 监听指定的端口
+        int port = 9000;
+        ServerSocket server = new ServerSocket(port);
+
+        //server将一直等待连接的到来
+        System.out.println("server将一直等待连接的到来");
+        Socket socket = server.accept();
+
+    }
 }
