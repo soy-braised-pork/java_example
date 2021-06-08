2230b10ef6d811e1bdc580246019d2cf7757b991
soy-braised-pork <2428784410@qq.com>
2021-06-08 09:40:03 +0800

    update

--- src/oop/Student.java
+++ src/oop/Student.java
@@ -77,4 +77,7 @@ public class Student extends Person {
 //            student.clazzId;
         }
     }
+
+
+
 }
---
+++ src/oop/Test.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Test extends SimpleTest {
+    public static void main(String[] args) {
+        SimpleTest s = new Test();
+
+    }
+}
+
+class SimpleTest {
+    private final String name = "zlx";
+
+}
