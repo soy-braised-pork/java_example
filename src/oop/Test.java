9371fe8e06242ae991d41d79739ff05c0abe51c0
soy-braised-pork <2428784410@qq.com>
2021-06-08 09:42:37 +0800

    update

--- src/oop/Test.java
+++ src/oop/Test.java
@@ -1,9 +1,14 @@
 package oop;
 
+/**
+ * 多态
+ */
+
+
 public class Test extends SimpleTest {
     public static void main(String[] args) {
         SimpleTest s = new Test();
-
+        System.out.println(s);
     }
 }
 
