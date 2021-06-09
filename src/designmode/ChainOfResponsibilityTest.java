7d9b61e85d39f2b67f1eb75deb4838f2ba3da460
soy-braised-pork <2428784410@qq.com>
2021-06-09 15:37:28 +0800

    update

--- src/designmode/ChainOfResponsibilityTest.java
+++ src/designmode/ChainOfResponsibilityTest.java
@@ -2,27 +2,5 @@ package designmode;
 
 import java.util.ArrayList;
 
-abstract class ChainOfResponsibility{
-    //下一个节点
-    protected ChainOfResponsibility nextNode;
-
-    public void setNextNode(ChainOfResponsibility nextNode) {
-        this.nextNode = nextNode;
-    }
-
-    public ChainOfResponsibility getNextNode() {
-        return nextNode;
-    }
-
-}
-
-class Chain extends ChainOfResponsibility{
-    private ArrayList<ChainOfResponsibility> arrayList=new ArrayList<>();
-    protected ChainOfResponsibility nextNode;
-
-    public Chain(){}
-}
-
-
 public class ChainOfResponsibilityTest {
 }
