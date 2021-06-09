6e1f3e0c4a9e64817a17db24f7fdaa050da6fb82
soy-braised-pork <2428784410@qq.com>
2021-06-09 15:38:54 +0800

    update

--- src/designmode/FecadeTest.java
+++ src/designmode/FecadeTest.java
@@ -1,54 +1,8 @@
 package designmode;
 
 
-import java.util.HashMap;
 
-class Util {
-    public static HashMap hashMap = new HashMap();
-
-    static {
-        hashMap.put("xxx.html", "#User.name,#User.sex");
-    }
-}
-
-class User {
-    private String name;
-    private String age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-}
-
-class Fecade {
-
-    public void render() {
-
-    }
-
-    public void write() {
-
-    }
-}
 
 public class FecadeTest {
-    public static void main(String[] args) {
-        Fecade fecade = new Fecade();
-        fecade.render();
-        fecade.write();
-        System.out.println();
-        System.out.println();
-    }
+
 }
