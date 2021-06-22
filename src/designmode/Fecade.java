b2ef02dd8a39120c49052d8efc381410242bcd5d
soy-braised-pork <2428784410@qq.com>
2021-06-22 09:57:36 +0800

    update

--- src/designmode/Fecade.java
+++ src/designmode/Fecade.java
@@ -1,53 +1,102 @@
 package designmode;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Locale;
 
-interface IToy{
-    void doplay();
-}
-
-
-class ToyGun_1 implements IToy{
-    private String name;
-    public ToyGun_1(String name){
-        this.name=name;
-    }
-
-    @Override
-    public void doplay() {
-        System.out.println("手枪 瞄准 开枪："+this.name);
-    }
-}
-
-class ToyGun_2 implements IToy{
-    private String name;
-    public ToyGun_2(String name){
-        this.name=name;
-    }
-    @Override
-    public void doplay() {
-        System.out.println("机枪 瞄准 开枪："+this.name);
-    }
-}
-
-class ToyGunFactory{
-    public static HashMap<String,Class> types=new HashMap();
+class Util{
+    public static HashMap page =new HashMap();
     static {
-        types.put("gun1",ToyGun_1.class);
-        types.put("gun2",ToyGun_2.class);
+        page.put("c:/xxx.html","id=#User.id,username=#User.name,age=#User.age,sex=#User.sex");
     }
-    public static Object create(String type,String name) throws Exception {
-        Class clz= (Class) types.get(type);
-        Constructor con=clz.getConstructor(String.class);
-        return con.newInstance(name);
+}
+
+class User{
+    private String sex;
+    private int id;
+    private int age;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+interface IDoProcess{
+    public String render(String path,Object u) throws Exception;
+    public String writehtml(String path);
+}
+
+
+class DoProcess implements IDoProcess{
+    @Override
+    public String render(String path, Object u) throws Exception {
+        String str= (String) Util.page.get(path);
+        User user= (User) u;
+        Class clz=user.getClass();
+        Field[] fs=clz.getDeclaredFields();
+        for (Field f:fs){
+            String mname=f.getName();
+            String tmp="get"+mname.substring(0,1).toUpperCase()+mname.substring(1,mname.length());
+//            System.out.println(tmp);
+            Method m=clz.getDeclaredMethod(tmp);
+            String tmpstr="#"+clz.getSimpleName()+"."+f.getName();
+//            System.out.println(tmpstr);
+            str=str.replaceAll(tmpstr,m.invoke(user)+"");
+        }
+        return str;
+    }
+
+    @Override
+    public String writehtml(String path) {
+        String str= (String) Util.page.get(path);
+        return str;
+    }
+}
+
+class FecadeTest{
+    private IDoProcess doProcess=new DoProcess();
+    public void render(String path,Object u) throws Exception {
+        String str=doProcess.render(path, u);
+        System.out.println(str);
+    }
+    public void writehtml(String path){
+        String str=doProcess.writehtml(path);
+        System.out.println(str);
     }
 }
 
 public class Fecade {
     public static void main(String[] args) throws Exception {
-        IToy toy= (IToy) ToyGunFactory.create("gun2","xxx");
-        toy.doplay();
+        FecadeTest f=new FecadeTest();
+        User u = new User();
+        u.setAge(22);
+        u.setId(1);
+        u.setSex("女");
+        f.render("c:/xxx.html",u);
+        f.writehtml("c:/xxx.html");
+        
     }
-
 }
--- src/designmode/Order.java
+++ src/designmode/Order.java
@@ -1,9 +1,33 @@
 package designmode;
 
+enum Stat{
+    ON,OFF
+}
 
+class Command{
+    private Stat stat;
+    public Command(Stat stat){
+        this.stat=stat;
+    }
+    public String exec(){
+        if (this.stat==Stat.ON){
+            return "打开了";
+        }else {
+            return "关上了";
+        }
+    }
+}
+
+class Process{
+    public void drive(Command c){
+        System.out.println(c.exec());
+    }
+}
 
 public class Order {
     public static void main(String[] args) {
-
+        Process p=new Process();
+        p.drive(new Command(Stat.ON));
+        p.drive(new Command(Stat.OFF));
     }
 }
