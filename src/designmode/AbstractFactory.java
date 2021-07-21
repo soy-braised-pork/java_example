1f768454fee04e2ef23f88dd040e759bf9a843e4
zhulixin <2428784410@qq.com>
2021-07-21 20:44:14 +0800

    update

--- src/designmode/AbstractFactory.java
+++ src/designmode/AbstractFactory.java
@@ -1,5 +1,17 @@
 package designmode;
 
+interface IToy{
+    void doplay();
+}
+
+class ToyGun_1 implements IToy{
+
+    @Override
+    public void doplay() {
+
+    }
+}
+
 public class AbstractFactory{
 
 }
